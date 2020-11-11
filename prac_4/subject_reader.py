38dc80255162405ad8b3947c8b6da69d96ca4344
Thiruvenkatesh Baladhandapani Palanisamy <74257620+Thiruvenkatesh-B-Palanisamy@users.noreply.github.com>
2020-11-11 22:37:46 +0530

    Update subject_reader.py

--- prac_4/subject_reader.py
+++ prac_4/subject_reader.py
@@ -1,1 +1,32 @@
+"""
+CP1404/CP5632 Practical
+Data file -> lists program
+"""
+
+FILENAME = "subject_data.txt"
+
+
+def main():
+    data = get_data()
+    display_subjects(data)
+
+
+def get_data():
+    data = []
+    input_file = open(FILENAME)
+    for line in input_file:
+
+        line = line.strip()
+        parts = line.split(',')
+        data.append(parts)
+    input_file.close()
+    return data
+
+
+def display_subjects(data):
+    for subject_data in data:
+        print("{} is taught by {} and has {} students".format(subject_data[0], subject_data[1], subject_data[2]))
+
+
+main()
 
