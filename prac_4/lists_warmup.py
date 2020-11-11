e7b84990703376d9e25721a86edf7e49ae374cc5
Thiruvenkatesh Baladhandapani Palanisamy <74257620+Thiruvenkatesh-B-Palanisamy@users.noreply.github.com>
2020-11-11 21:26:12 +0530

    Update lists_warmup.py

--- prac_4/lists_warmup.py
+++ prac_4/lists_warmup.py
@@ -1,1 +1,12 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+numbers[0] = "ten"
+print(numbers)
+numbers[-1] = 0
+print(numbers)
+num_slice = numbers[2:]
+print(num_slice)
+for number in numbers:
+    if number == 9:
+        print("yes")
+        break
 
