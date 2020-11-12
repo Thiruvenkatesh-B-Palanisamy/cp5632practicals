77b6fe4ba0059273c81c847546d2cffca11907c2
thiru-4738 <64815362+thiru-4738@users.noreply.github.com>
2020-11-12 16:05:39 +0530

    added img

--- prac_5/state_names.py
+++ prac_5/state_names.py
@@ -1,1 +1,14 @@
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales",
+                "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria",
+                "TAS": "Tasmania"}
+
+
+state_code = input("Enter short state: ").upper()
+while state_code != "":
+    if state_code in CODE_TO_NAME:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+    else:
+        print("Invalid short state")
+    state_code = input("Enter short state: ").upper()
 
