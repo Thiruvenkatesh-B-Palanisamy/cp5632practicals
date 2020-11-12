979d628f2f7086dbcd07bde3aa6d7b09e3ef9294
Thiruvenkatesh B Palanisamy <>
2020-11-12 21:53:31 +0530

    finished

--- prac_5/word_occurrences.py
+++ prac_5/word_occurrences.py
@@ -1,1 +1,16 @@
+word_count_dict = {}
+text = input("Text").split()
+for word in text:
+    if word in word_count_dict:
+        word_count_dict[word] += 1
+    else:
+        word_count_dict[word] = 1
+print(word_count_dict)
+text = list(word_count_dict.keys())
+text.sort()
+length_align = max(len(word) for word in text)
+for word in text:
+    print("{:{}} : {}".format(word, length_align, word_count_dict[word]))
+
+
 
