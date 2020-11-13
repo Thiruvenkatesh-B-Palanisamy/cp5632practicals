e8266529059b71803cbb6fedee72dd0afb23ea5f
Thiruvenkatesh B Palanisamy <>
2020-11-13 20:15:42 +0530

    format changes

--- prac_5/word_occurrences.py
+++ prac_5/word_occurrences.py
@@ -1,11 +1,10 @@
 word_count_dict = {}
-text = input("Text").split()
+text = input("Text: ").split()
 for word in text:
     if word in word_count_dict:
         word_count_dict[word] += 1
     else:
         word_count_dict[word] = 1
-print(word_count_dict)
 text = list(word_count_dict.keys())
 text.sort()
 length_align = max(len(word) for word in text)
