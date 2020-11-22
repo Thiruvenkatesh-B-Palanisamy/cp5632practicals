170589202cbf86998f4ea57d79d8fd5f397eccce
Thiruvenkatesh B Palanisamy <>
2020-11-23 01:11:13 +0530

    car completed

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,12 +4,16 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
         self.odometer = 0
+
+    def __str__(self):
+        return "{} fuel = {}, odometer = {}".format(self.name, self.fuel, self.odometer)
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
--- prac_06/used_car.py
+++ prac_06/used_car.py
@@ -1,12 +1,12 @@
 """CP1404/CP5632 Practical - Client code to use the Car class."""
 # Note that the import has a folder (module) in it.
 
-from prac_06.car import Car
+from car import Car
 
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("Mycar", 180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
@@ -15,5 +15,13 @@ def main():
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
 
+    limo = Car("Limo", 100)
+    limo.add_fuel(20)
+    print(limo.fuel)
+    limo.drive(115)
+    print(limo.odometer)
+    print(limo)
+
 
 main()
+
