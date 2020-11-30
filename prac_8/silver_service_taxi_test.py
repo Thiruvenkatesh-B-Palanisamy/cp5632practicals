7d74598c8f2330fdd71b04cd3bec24880b32cf7c
Thiruvenkatesh B Palanisamy <>
2020-11-30 23:16:54 +0530

    changes format

--- prac_8/silver_service_taxi_test.py
+++ prac_8/silver_service_taxi_test.py
@@ -2,11 +2,10 @@ from silver_service_taxi import SilverServiceTaxi
 
 
 def main():
-    """Test SilverServiceTaxi."""
     taxi = SilverServiceTaxi("Test Fancy Taxi", 100, 2)
     taxi.drive(18)
     print(taxi)
     print(taxi.get_fare())
 
-main()
 
+main()
